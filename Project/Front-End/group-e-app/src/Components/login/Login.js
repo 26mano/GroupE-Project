3d9815be1560321f849d7b45f477bbae280121c9
Shiva <guptareshu37@gmail.com>
2022-08-04 09:37:59 +0530

    3 aug

--- Project/Front-End/group-e-app/src/Components/login/Login.js
+++ Project/Front-End/group-e-app/src/Components/login/Login.js
@@ -1,100 +1,55 @@
-import { Box, Button, Container, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, Stack, TextField, Typography } from '@mui/material';
-import React, { useState } from 'react';
-import UserLog from './loguser.svg';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import { Link, NavLink } from 'react-router-dom';
-import "./Login.css";
+import React, { useState } from 'react'
 
+import { useNavigate } from 'react-router-dom'
 
-function Login(props) {
+const Login=()=>{
+  const history = useNavigate();
 
-    const [values, setValues] = useState({
-        password: '',
-        showPassword: false,
-    });
-    const [mail, setMail] = useState("");
+  const [inval, setInval] = useState({
+      email: "",
+      password: ""
+  })
 
-    const handleEmailChange = (e) => {
-        setMail(e.target.value);
-    }
+  const [data, setData] = useState([]);
+  console.log(inval);
+  const InputEvent = (e) => {
 
-    const handleChange = (prop) => (event) => {
-        setValues({ ...values, [prop]: event.target.value });
-      };
+    const { value, name } = e.target;
 
-    const handleClickShowPassword = () => {
-        setValues({
-          ...values,
-          showPassword: !values.showPassword,
-        });
-      };
+    setInval(() => {
+        return {
+            ...inval,
+            [name]: value
+        }
+    })
 
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-      };
-    return (
-        <div  >
-            <div  className='bg-image'  > </div>
-            <Stack sx={{position:"absolute",width:"100%", height:"100vh", justifyContent:"center" , alignItems:"center" , zIndex:20,  transform: "translate(-0%, -100%)"}} >
-
-              <Stack  justifyContent="center" >
-                  <Box sx={{ width:"80%" ,  boxShadow:"1px 1px 6px #ccc" ,  bgcolor:"rgba(250, 252, 254, 0.7) " , padding:"20px" , borderRadius:"5px", py:7 , px:5}} >
-
-                      <Container sx={{ display:"grid" , width:"100%" }}>
-                          <Stack sx={{mb:2}} >
-                          <img src={UserLog} alt="Profile-img" width={100} style={{ alignSelf:"center" }}  />
-                          <Typography variant='h6' fontWeight="bold" textAlign="center" >LOG-IN</Typography>
-                          </Stack>
-
-                          
-                        <TextField  
-                        variant='outlined' 
-                        type="email" 
-                        label="Enter Your Email ID" 
-                        value={mail}
-                        pattern="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
-                        onChange={handleEmailChange}
-                        sx={{ my:"10px" }} 
-                        required/>
-                      
-                        <FormControl sx={{ my:"10px", width:"100%" }} variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                          <OutlinedInput
-                            id="outlined-adornment-password"
-                               type={values.showPassword ? 'text' : 'password'}
-                            value={values.password}
-                            onChange={handleChange('password')}
-                            endAdornment={
-                             <InputAdornment position="end">
-                           <IconButton
-                                aria-label="toggle password visibility"
-                                 onClick={handleClickShowPassword}
-                                 onMouseDown={handleMouseDownPassword}
-                                 edge="end"
-                                   >
-                               {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                             </InputAdornment>
-                                 }
-                            label="Password"
-                              />
-                          </FormControl>
-                            
-                          
-                        <NavLink  to="/AllBlog" style={{ textDecoration:"none" }} >
-                        <Button variant='contained'  fullWidth  sx={{ my:"10px" }} > Login </Button>
-                        </NavLink >
-                          
-                      </Container>
-                      <Typography variant='subtitle2' mt={2} textAlign="center" > If you didn't have account : Register <span ><Link to="/Register" >here</Link>  </span></Typography>
-                  </Box>
-              </Stack>
-
-            </Stack>
-            
-        </div>
-    );
 }
 
-export default Login;
+const addData = (e) => {
+    e.preventDefault();
+const getuser = localStorage.getItem("user");
+    // console.log(getuser);
+
+    const { email, password } = inval;
+ console.log(inval);
+ const userdata = JSON.parse(getuser);
+const userone = userdata.filter((el)=>{
+return el.email === email && el.password === password
+})
+console.log(userdata);
+}
+  return(
+   
+    <>
+  <form onSubmit={addData}>
+  <input type="email" placeholder="Enter the Email"  name="email"  value={inval.email}      onChange={InputEvent}  />
+  <br/><br/>
+ 
+  <input type="password" placeholder="Enter the password" name="password"  value={inval.password}   onChange={InputEvent}/>
+  <br/><br/>
+  <button type="submit">submit</button>
+  </form>
+    </>
+  )
+}
+export default Login
--- Project/Front-End/group-e-app/src/Components/login/Register/Register.js
+++ Project/Front-End/group-e-app/src/Components/login/Register/Register.js
@@ -1,157 +1,68 @@
-import { Box, Button, Container, FormControl, FormGroup, IconButton, InputAdornment, InputLabel, OutlinedInput, Stack, TextField, Typography } from '@mui/material';
-import React, { useState } from 'react';
-// import UserLog from './loguser.svg';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import { Link } from 'react-router-dom';
-import "./Register.css";
-// import { Icon } from '@iconify/react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import Nav from "../../Nav";
 
-function Register(props) {
-    const [values, setValues] = useState({
-        password: '',
-        showPassword: false,
-    });
+function Register() {
 
-    const [confirm, setConfirm] = useState({
-        confirmPassword: '',
-        showConfirmPassword: false,
-    });
-
-    const handleChange = (prop) => (event) => {
-        setValues({ ...values, [prop]: event.target.value });
-      };
-
-    const handleClickShowPassword = () => {
-        setValues({
-          ...values,
-          showPassword: !values.showPassword,
-        });
-      };
-
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-      };
-
-    const handleConfirm = (prop) => (event) => {
-        setConfirm({ ...confirm, [prop]: event.target.value });
-      };
-
-    const handleClickShowConfirmPassword = () => {
-        setConfirm({
-          ...confirm,
-          showConfirmPassword: !confirm.showConfirmPassword,
-        });
-      };
-
-    const handleMouseDownConfirmPassword = (event) => {
-        event.preventDefault();
-      };
-
-
-
-    return (
-        <div  >
-            <div  className='bg-image'  > </div>
-            <Stack sx={{position:"absolute",width:"100%", height:"100%", justifyContent:"center" , alignItems:"center" , zIndex:10,  transform: "translate(-0%, -100%)"}} >
-
-              <Stack   >
-                  <Box sx={{ width:"90%" , height:"80%" , boxShadow:"1px 1px 6px #ccc" ,  bgcolor:"rgba(250, 252, 254, 0.7) " , padding:"20px" , borderRadius:"5px", py:7 , px:5}} >
-                     
-
-                      <Container sx={{ display:"grid" , width:"100%" }}>
-                          <Stack sx={{mb:2}} >
-                          {/* <img src={UserLog} alt="Profile-img" width={100} style={{ alignSelf:"center" }}  /> */}
-                          <Typography variant='h6' fontWeight="bold" textAlign="center" >REGISTER</Typography>
-                          </Stack>
-               <FormGroup sx={{ width:"100%" }} variant="outlined" >
-
-                        <TextField  
-                        variant='outlined' 
-                        type= "text" 
-                        name="name"
-                        label= "Enter Your Name"
-                        onChange={handleChange}
-                        sx={{ mb:"5px"  }} />
-
-                        <TextField  
-                        variant='outlined' 
-                        type= "tel" 
-                        name="number"
-                        label= "Enter Your Mobile Number"
-                        onChange={handleChange}
-                        sx={{ mb:"5px"}} />
-
-                        <TextField  
-                        variant='outlined' 
-                        type= "email" 
-                        name="email"
-                        label= "Enter Your Email"
-                        onChange={handleChange}
-                        sx={{ mb:"5px" }} />                 
-                 
-                        <FormControl sx={{  width:"100%" }} variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                          <OutlinedInput
-                            id="outlined-adornment-password"
-                            type={values.showPassword ? 'text' : 'password'}
-
-                            value={values.password}
-
-                            onChange={handleChange('password')}
-
-                            endAdornment={
-                             <InputAdornment position="end">
-                           <IconButton
-                                aria-label="toggle password visibility"
-                                 onClick={handleClickShowPassword}
-                                 onMouseDown={handleMouseDownPassword}
-                                 edge="end"
-                                   >
-                               {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                             </InputAdornment>
-                                 }
-                            label="Password"
-                              />
-
-                          </FormControl>
-
-                      <FormControl sx={{ my:"10px", width:"100%" }} variant="outlined">
-                         <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
-                          <OutlinedInput
-                            id="outlined-adornment-password"
-                            type={confirm.showConfirmPassword ? 'text' : 'password'}
-                            value={confirm.confirmPassword}
-                            onChange={handleConfirm('confirmPassword')}
-                            endAdornment={
-                             <InputAdornment position="end" >
-                           <IconButton
-                                aria-label="toggle password visibility"
-                                 onClick={handleClickShowConfirmPassword}
-                                 onMouseDown={handleMouseDownConfirmPassword}
-                                 edge="end"
-                                   >
-                               {confirm.showConfirmPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                             </InputAdornment>
-                                 }
-                            label="Confirm Password"
-                              />
-                          </FormControl>
-
-                      </FormGroup>     
-                        
-                          <Button variant='contained' fullWidth  sx={{ mb:"5px" }} > Register </Button>
-                      </Container>
-                      <Typography variant='subtitle2' mt={2} textAlign="center" > Already have a account <span ><Link to="/Login" >Login</Link>  </span></Typography>
-                  </Box>
-              </Stack>
-
-            </Stack>
-            
-        </div>
-    );
+const[inval,setInval] = useState({
+  name:"",
+  email:"",
+  phone:"",
+  password:""
+})
+const[data,setData] = useState([]);
+ const history =  useNavigate();
+const InputEvent=(e)=>{
+  e.preventDefault();
+  const{value,name} = e.target
+  setInval(()=>{
+    return{
+      ...inval,
+      [name]:value
+    }
+  })
 }
+const addData=(e)=>{
+  e.preventDefault();
+  const{name,email,phone,password} = inval;
+  console.log(inval);
+  if(email==="")
+  {
+    alert("email is require");
+  }
+  else if(password==="")
+  {
+    alert("password is require")
+  }else if(password.length<2)
+  {
+    alert("password is greater")
+  }
+  else{
+    console.log("data add hai")
+    localStorage.setItem("user",JSON.stringify(...data,inval));
+    history("/Login");
+  }
 
+}
+  return(
+<div>
+  
+  {/* Register form start */}
+  <section>
+<form onSubmit={addData}>
+  <input type="text" placeholder="Enter the name"  name="name" value={inval.name}  onChange={InputEvent}/>
+  <br/><br/>
+  <input type="email" placeholder="Enter the Email"  name="email"  value={inval.email}      onChange={InputEvent}  />
+  <br/><br/>
+  <input type="number" placeholder="Enter the phone"  name="phone" value={inval.phone}   onChange={InputEvent}/>
+  <br/><br/>
+  <input type="password" placeholder="Enter the password" name="password"  value={inval.password}   onChange={InputEvent}/>
+  <br/><br/>
+  <button type="submit">submit</button>
+</form>
+  </section>
+</div>
+
+  )
+}
 export default Register;
